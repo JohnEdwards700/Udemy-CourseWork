94ae9a3436a02ab46f156c44491eae9da68e4132
John Edwards <johntristanedwards@gmail.com>
2024-07-30 08:24:20 -0500

    **Initilized Linked List**

---
+++ LinkedList.py
@@ -0,0 +1,12 @@
+class Node:
+    
+    def __init__(self, value):
+        node = value
+        node.next = None
+        
+class LinkedList:
+    def __init__(self, value):
+        newNode = Node(value)
+        self.head = newNode
+        self.tail = newNode
+        self.length = 1
